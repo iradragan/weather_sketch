f9fdd39f7f0fe05dc1151f4ed5ae145640109a7f
iradragan <iradragan@gmail.com>
2015-12-17 16:12:27 +0000

    scripts added

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
-    
-    $(function() {
+$(function() {
         navigator.geolocation.getCurrentPosition(locationSuccess, locationError);  
     });
 
@@ -7,19 +6,13 @@
     {
         var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?lat='+position.coords.latitude+
         '&lon='+position.coords.longitude+'&APPID=b5eb1f7fc74c772602241ba72db33987&callback=?';
-   $.getJSON(weatherAPI, function(response) 
-   {
-     console.log(response);
-     $('#loc').html(response.city.name);
-     $('#currenttemp').html(parseInt(response.list[0].main.temp - 273.15)+'&deg;C');
-     for(var i = 1; i < 40; i = i + 8)
-     {
-       $('#temp').append('<tr><td>Date: '+ response.list[i].dt_txt+'</td><td>'+
-       parseInt(response.list[i].main.temp - 273.15)+'&deg;C</td></tr>');
-     }
-   });
-}
+       $.getJSON(weatherAPI, function(response) 
+       {
+         $('#loc').html(response.city.name);
+         $('#currenttemp').html(parseInt(response.list[0].main.temp - 273.15)+'&deg;C');
+       });
+    }
              
-function locationError(error) {
-   console.warn('Error:' + error.message);
-}
+    function locationError(error) {
+    console.warn('Error:' + error.message);
+    }
